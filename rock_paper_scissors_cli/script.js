f37723d2b4f15b809bd8ced4b395e2f0bca3fc16
Mokili Promise Pierre <132935466+sanepierr@users.noreply.github.com>
2025-03-01 14:48:59 +0300

    fixed_score+update

--- rock_paper_scissors_cli/script.js
+++ rock_paper_scissors_cli/script.js
@@ -1,3 +1,4 @@
+
 function getHumanChoice(){
     let humanChoice = prompt("Choose rock, paper or scissors: ");
     return humanChoice;
@@ -18,50 +19,70 @@ function getComputerChoice(){
     return computerChoice;
 }
 
-function result(){
-    let _humanChoice = getHumanChoice();
-    switch (_humanChoice.toLowerCase().trim()){
-        case "rock":
-            let computerChoice1 =  getComputerChoice();
-            if (computerChoice1 === "rock"){
-                alert("It's a tie!");
-            }
-            else if (computerChoice1 === "scissors"){
-                alert("You win!");
-            }
-            else if (computerChoice1 === "paper"){
-                alert("You lose");
-            }
-            break;
-    
-        case "paper":
-            let computerChoice2 =  getComputerChoice();
-            if (computerChoice2 === "paper"){
-                alert("It's a tie!");
-            }
-            else if (computerChoice2 === "scissors"){
-                alert("You lose!");
-            }
-            else if (computerChoice2 === "rock"){
-                alert("You win!");
-            }
-            break;
-    
-        case "scissors":
-            let computerChoice3 =  getComputerChoice();
-            if (computerChoice3 === "rock"){
-                alert("You lose!");
-            }
-            else if (computerChoice3 === "scissors"){
-                alert("It's a tie!");
-            }
-            else if (computerChoice3 === "paper"){
-                alert("You win");
-            }
-            break;
+
+function playGame(){
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(){
+        let _humanChoice = getHumanChoice();
+        switch (_humanChoice.toLowerCase().trim()){
+            case "rock":
+                let computerChoice1 =  getComputerChoice();
+                if (computerChoice1 === "rock"){
+                    alert("It's a tie!");
+                }
+                else if (computerChoice1 === "scissors"){
+                    humanScore += 1;
+                    alert("You win!");
+                }
+                else if (computerChoice1 === "paper"){
+                    ++computerScore;
+                    alert("You lose");
+                }
+                break;
+        
+            case "paper":
+                let computerChoice2 =  getComputerChoice();
+                if (computerChoice2 === "paper"){
+                    alert("It's a tie!");
+                }
+                else if (computerChoice2 === "scissors"){
+                    ++computerScore;
+                    alert("You lose!");
+                }
+                else if (computerChoice2 === "rock"){
+                    ++humanScore;
+                    alert("You win!");
+                }
+                break;
+        
+            case "scissors":
+                let computerChoice3 =  getComputerChoice();
+                if (computerChoice3 === "rock"){
+                    ++computerScore;
+                    alert("You lose!");
+                }
+                else if (computerChoice3 === "scissors"){
+                    alert("It's a tie!");
+                }
+                else if (computerChoice3 === "paper"){
+                    ++humanScore;
+                    alert("You win");
+                }
+                break;
+        }
     }
+
+    for(let i = 0; i < 5; i++){
+        playRound();
+    }
+    alert(`player score: ${humanScore}\ncomputer score: ${computerScore}`);
 }
 
 
-document.body.addEventListener('load', result());
+
+
+document.body.addEventListener('load', playGame());
 
