c321f6e8146e1cddfe5a9c3ffbf458070b3f04ef
Mokili Promise Pierre <132935466+sanepierr@users.noreply.github.com>
2025-03-15 04:41:57 +0300

    adding+scores

--- rock_paper_scissors_cli/script.js
+++ rock_paper_scissors_cli/script.js
@@ -1,14 +1,18 @@
-const _rock = document.getElementsByClassName('rock-button')[0];
-const _paper = document.getElementsByClassName('paper-button')[0];
-const _scissors = document.getElementsByClassName('scissors-button')[0];
+const rock = document.getElementsByClassName("rock")[0];
+const paper = document.getElementsByClassName("paper")[0];
+const scissors = document.getElementsByClassName("scissors")[0];
+const playerScore = document.querySelector('.playerScore');
+const AiScore = document.querySelector('.computerScore');
+
+
 
 function getComputerChoice(){
     const randomValue = Math.random();
     let computerChoice;
-    if (0 <= randomValue <= 0.3){
+    if (randomValue <= 0.3){
         computerChoice = "rock"; 
     }
-    else if (0.3 < randomValue <= 0.6){
+    else if (randomValue <= 0.6){
         computerChoice = "paper";
     }
     else {
@@ -16,42 +20,72 @@ function getComputerChoice(){
     }
     return computerChoice;
 }
+let humanScore = 0;
+let computerScore = 0;
 
-_rock.addEventListener('click', ()=>{
-    let computerChoice =  getComputerChoice();
-    if (computerChoice === "rock"){
-        alert("It's a tie!");
+function playRound(humanChoice){
+    switch (humanChoice){
+        case "rock":
+            let computerChoice1 =  getComputerChoice();
+            if (computerChoice1 === "rock"){
+                alert("It's a tie!");
+            }
+            else if (computerChoice1 === "scissors"){
+                humanScore += 1;
+                alert("You win!");
+            }
+            else if (computerChoice1 === "paper"){
+                ++computerScore;
+                alert("You lose");
+            }
+            AiScore.textContent = 'Computer score: ${computerScore}';
+            playerScore.textContent = "Player score: ${humanScore}";
+            break;
+    
+        case "paper":
+            let computerChoice2 =  getComputerChoice();
+            if (computerChoice2 === "paper"){
+                alert("It's a tie!");
+            }
+            else if (computerChoice2 === "scissors"){
+                ++computerScore;
+                alert("You lose!");
+            }
+            else if (computerChoice2 === "rock"){
+                ++humanScore;
+                alert("You win!");
+            }
+            AiScore.textContent = 'Computer score: ${computerScore}';
+            playerScore.textContent = "Player score: ${humanScore}";
+            break;
+    
+        case "scissors":
+            let computerChoice3 =  getComputerChoice();
+            if (computerChoice3 === "rock"){
+                ++computerScore;
+                alert("You lose!");
+            }
+            else if (computerChoice3 === "scissors"){
+                alert("It's a tie!");
+            }
+            else if (computerChoice3 === "paper"){
+                ++humanScore;
+                alert("You win");
+            }
+            AiScore.textContent = "Computer score: ${computerScore}";
+            playerScore.textContent = "Player score: ${humanScore}";
+            break;
     }
-    else if (computerChoice === "scissors"){
-        alert("You win!");
+    if (humanScore === 5){
+        alert('You have won the game');
     }
-    else if (computerChoice === "paper"){
-        alert("You lose");
+    else {
+        alert('You have lost the game');
     }
-});
+}
+    rock.addEventListener("click", ()=>playRound("rock"));
+    paper.addEventListener("click", ()=>playRound("paper"));
+    scissors.addEventListener("click", ()=>playRound("scissors"));
 
-_paper.addEventListener('click', ()=>{
-    let computerChoice =  getComputerChoice();
-    if (computerChoice === "paper"){
-        alert("It's a tie!");a
-    }
-    else if (computerChoice === "scissors"){
-        alert("You lose!");
-    }
-    else if (computerChoice === "rock"){
-        alert("You win!");
-    }
-});
 
-_scissors.addEventListener('click', ()=>{
-    let computerChoice =  getComputerChoice();
-    if (computerChoice === "rock"){
-        alert("You lose!");
-    }
-    else if (computerChoice === "scissors"){
-        alert("It's a tie!");
-    }
-    else if (computerChoice === "paper"){
-        alert("You win");
-    }
-});
+
